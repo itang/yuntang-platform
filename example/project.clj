f45cca343e597f9a9536a63f1808b5cd4397acf0
itang <live.tang@gmail.com>
2013-07-30 17:25:34 +0800

    update

--- example/project.clj
+++ example/project.clj
@@ -12,13 +12,13 @@
                  [clj-captcha "0.1.1-SNAPSHOT"]
                  [ansj_seg/ansj_seg "1.0"]]
   :repositories [["itang-repo" "http://itang.github.io/maven-repo"]]
-  :profiles {:dev {:plugins [[lein-ancient "0.4.3"]  ;$ lein ancient > check your project for outdated dependencies and plugins
+  :profiles {:dev {:plugins [[lein-ancient "0.4.4"]  ;$ lein ancient > check your project for outdated dependencies and plugins
                              [codox "0.6.4"]         ;$ lein doc
                              [lein-ring "0.8.6"]     ;$ lein ring server
                              [lein-pprint "1.1.1"]   ;$ lein pprint | lein with-profile 1.4 pprint
                              [lein-cljsbuild "0.3.2"];$ lein cljsbuild [once auto clean repl-listen repl-rhino]
                              [lein-environ "0.4.0"]
-                             [lein-localrepo "0.5.1"]
+                             [lein-localrepo "0.5.2"]
                              [lein-checkall "0.1.1"]]
                    :dependencies [[ring-mock "0.1.5"]
                                   [org.clojure/clojurescript "0.0-1847"]]}
