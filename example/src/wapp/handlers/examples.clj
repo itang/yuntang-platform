5d52fb4822b72f3910be44f01d325ee45b41ef26
itang <live.tang@gmail.com>
2013-09-05 16:07:07 +0800

    cljwtang-Issue #11:

--- example/src/wapp/handlers/examples.clj
+++ example/src/wapp/handlers/examples.clj
@@ -2,7 +2,6 @@
   (:import org.ansj.splitWord.analysis.ToAnalysis
            java.util.Date)
   (:require [clojure.core.match :refer [match]]
-            [clojure.tools.logging :as log]
             [cljtang.core :refer :all]
             [coercer.core :refer [coerce]]
             [noir.session :refer [*noir-flash* *noir-session* mem] :as session]
@@ -10,108 +9,101 @@
             [clj-captcha.core :refer [captcha-response-correc?]]
             [cljwtang.lib :refer :all]))
 
-(defhandler index [req]
-  (view "examples/index"
-               {:foo (tower/t :example/foo)
-                :locale (get-in req [:headers "accept-language"])}))
+(with-routes examples-routes
+  (defhandler-meta {:path "/examples" :fp-name "examples index" :method :get}
+    examples-index [req]
+    (view "examples/index"
+          {:foo (tower/t :example/foo)
+           :locale (get-in req [:headers "accept-language"])}))
 
-(defhandler ajax [age]
-  (json-message true
-                "Hello, World!"
-                {:age (inc (coerce age Integer)) :now (moment-format)}))
+  (defhandler-meta {:path "/examples/ajax" :method :get}
+    ajax [age]
+    (json-success-message "Hello, World!"
+                          {:age (inc (coerce age Integer)) :now (moment-format)}))
 
-(defhandler ajaxform [req]
-  (json-success-message "" (:params req)))
+  (defhandler-meta {:path "/examples/ajax_form/ajaxform" :method :post}
+    ajaxform [req]
+    (json-success-message "" (:params req)))
 
-(defhandler ajaxfileupload [req]
-  (let [p (:params req)
-        file (:file p)
-        filename (:filename file)
-        ^java.io.File tempfile (:tempfile file)]
-    (log/debug p)
-    (when tempfile
-      (upload-file tempfile filename))
-    (json-success-message "" {:user (:username p)
-                              :filesize (:size file)
-                              :filename filename
-                              :content-type (:content-type file)
-                              :tempfile (some-> tempfile .getName)})))
+  (defhandler-meta {:path "/examples/ajax_form/ajaxfileupload" :method :post}
+    ajaxfileupload [req]
+    (let [p (:params req)
+          file (:file p)
+          filename (:filename file)
+          ^java.io.File tempfile (:tempfile file)]
+      (debug p)
+      (when tempfile
+        (upload-file tempfile filename))
+      (json-success-message "" {:user (:username p)
+                                :filesize (:size file)
+                                :filename filename
+                                :content-type (:content-type file)
+                                :tempfile (some-> tempfile .getName)})))
 
-(defhandler ansj [source]
-  (->> (ToAnalysis/paser source)
-    str
-    (json-success-message "操作完成")))
+  (defhandler-meta {:path "/examples/ansj" :method :get}
+    ansj [source]
+    (->> (ToAnalysis/paser source)
+         str
+         (json-success-message "操作完成")))
 
-(defhandler fileupload-index []
-  (view "examples/fileupload"
-               {:noir-flash @*noir-flash*
-                :noir-session @*noir-session*
-                :mem @mem}))
+  (defhandler-meta {:path "/examples/fileupload" :fp-name "example fileupload" :method :get}
+    fileupload-index []
+    (view "examples/fileupload"
+          {:noir-flash @*noir-flash*
+           :noir-session @*noir-session*
+           :mem @mem}))
 
-(defhandler fileupload [req]
-  (Thread/sleep (rand-nth (range 500 1600 100))) ;; mock
-  (log/debug (:params req))
-  (log/debug (pr-str (:multipart-params req)))
-  (let [files (get-in req [:multipart-params "file"])]
-    (log/debug "files" files)
-    (if-not (or (map? files) (sequential? files))
-      (json-error-message "no files")
-      (let [fmap (if (sequential? files) files [files])]
-        (session/flash-put! :msg (success-message "上传成功!"))
-        (if (ajax? req)
-          (json-success-message ""
-                                {:files (map #(dissoc % :tempfile) fmap)
-                                 :user (get-in req [:params :user])})
-          (redirect "/examples/fileupload"))))))
+  (defhandler-meta {:path "/examples/fileupload" :method :post}
+    fileupload [req]
+    (Thread/sleep (rand-nth (range 500 1600 100))) ;; mock
+    (debug (:params req))
+    (debug (pr-str (:multipart-params req)))
+    (let [files (get-in req [:multipart-params "file"])]
+      (debug "files" files)
+      (if-not (or (map? files) (sequential? files))
+        (json-error-message "no files")
+        (let [fmap (if (sequential? files) files [files])]
+          (session/flash-put! :msg (success-message "上传成功!"))
+          (if (ajax? req)
+            (json-success-message ""
+                                  {:files (map #(dissoc % :tempfile) fmap)
+                                   :user (get-in req [:params :user])})
+            (redirect "/examples/fileupload"))))))
 
-(defn ft []
-  (flash-msg "flash msg")
-  (redirect "/examples/show"))
+  (defhandler-meta {:path "/examples/va-captcha" :method :post}
+    va-captcha [captcha req]
+    (let [correc? (captcha-response-correc? captcha)]
+      (json-message correc?
+                    (if correc? "验证码填写正确" "验证码填写有误")
+                    {:is-response-correc correc?})))
 
-(defn show []
-  (str "msg:" (flash-msg) "<a href='/examples/ft'>++</a>"))
+  (defhandler-meta {:path "/examples/match" :method :get}
+    match-demo []
+    (->> (for [n (range 1 101)]
+           (match [(mod n 3) (mod n 5)]
+                  [0 0] [n "FizzBuzz"]
+                  [0 _] [n "Fizz"]
+                  [_ 0] [n "Buzz"]
+                  :else [n "NON"]))
+         (filter #(not= "NON" (second %)))
+         (into {})
+         (json-message true "" )))
 
-(defhandler va-captcha [captcha req]
-  (let [correc? (captcha-response-correc? captcha)]
-    (json-message correc?
-                  (if correc? "验证码填写正确" "验证码填写有误")
-                  {:is-response-correc correc?})))
+  )
 
-(def v-def "hello")
-(def v-delay (delay "hello"))
-
-;; 7789.42 [#/sec] (mean)
-(defhandler perf-def []
-  (html v-def))
-
-;; ab -n 100000 -c 10 -k => 7576.80 [#/sec]
-(defhandler perf-delay []
-  (html @v-delay))
-
-(defhandler match-demo []
-  (->> (for [n (range 1 101)]
-         (match [(mod n 3) (mod n 5)]
-                [0 0] [n "FizzBuzz"]
-                [0 _] [n "Fizz"]
-                [_ 0] [n "Buzz"]
-                :else [n "NON"]))
-    (filter #(not= "NON" (second %)))
-    (into {})
-    (json-message true "" )))
-
-(defroutes examples-routes
-  (context "/examples" []
-    (GET "/perf/def" [] perf-def)
-    (GET "/perf/delay" [] perf-delay)
-    (GET "/" [] index)
-    (GET "/ft" [] ft)
-    (GET "/show" [] show)
-    (GET "/ajax" [] ajax)
-    (POST "/ajax_form/ajaxform" [] ajaxform)
-    (POST "/ajax_form/ajaxsubmit" [] ajaxform)
-    (POST "/ajax_form/ajaxfileupload" [] ajaxfileupload)
-    (GET "/ansj" [] ansj)
-    (GET "/fileupload" [] fileupload-index)
-    (POST "/fileupload" [] fileupload)
-    (POST "/va-captcha" [] va-captcha)
-    (GET "/match" [] match-demo)))
+#_(defroutes examples-routes
+    (context "/examples" []
+             (GET "/perf/def" [] perf-def)
+             (GET "/perf/delay" [] perf-delay)
+             (GET "/" [] index)
+             (GET "/ft" [] ft)
+             (GET "/show" [] show)
+             (GET "/ajax" [] ajax)
+             (POST "/ajax_form/ajaxform" [] ajaxform)
+             (POST "/ajax_form/ajaxsubmit" [] ajaxform)
+             (POST "/ajax_form/ajaxfileupload" [] ajaxfileupload)
+             (GET "/ansj" [] ansj)
+             (GET "/fileupload" [] fileupload-index)
+             (POST "/fileupload" [] fileupload)
+             (POST "/va-captcha" [] va-captcha)
+             (GET "/match" [] match-demo)))
--- example/src/wapp/module.clj
+++ example/src/wapp/module.clj
@@ -1,26 +1,18 @@
 (ns wapp.module
   (:require [cljwtang.lib :refer :all]
-            [wapp.handlers.examples :refer [examples-routes]]))
-
-(def fp-examples
-  (new-funcpoint {:name "examples"
-                  :url "/examples"}))
-
-(def fp-examples-fileupload
-  (new-funcpoint {:name "examples.fileupload"
-                  :url "/examples/fileupload"}))
+            [wapp.handlers.examples :refer :all]))
 
 (def module
   (new-ui-module
     {:name "example app"
-     :routes [examples-routes]
-     :fps [fp-examples fp-examples-fileupload]
+     :routes [@examples-routes]
+     :fps [examples-index-fp fileupload-index-fp]
      :menus (maps->menus
               [{:id "examples"
                 :name "Examples"
-                :funcpoint fp-examples
+                :funcpoint examples-index-fp
                 :parent "dashboard"}
                {:name "File Upload"
-                :funcpoint fp-examples-fileupload
+                :funcpoint fileupload-index-fp
                 :parent "dashboard"}])
      :snippets-ns ['wapp.views.snippets]}))
