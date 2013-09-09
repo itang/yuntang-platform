3f8a3ee19b4331714109801d4bff14b0899c719d
itang <live.tang@gmail.com>
2013-09-09 20:54:55 +0800

    cljwtang-Issue #6

--- example/src/wapp/server.clj
+++ example/src/wapp/server.clj
@@ -1,17 +1,9 @@
 (ns wapp.server
-  (:require [cljwtang.lib :refer :all]
-            [wapp.config :as wappcofig]
-            [yuntang.user.core :as user-core])
-#_(:require
-             [yuntang.user.module :as user-module]
-             [yuntang.layout.module :as layout-module]
-             [yuntang.admin.module :as admin-module]
-             [yuntang.modules.captcha.module :as captcha-module]
-             [yuntang.modules.common.module :as common-module]
-             [wapp.module :as wapp])
+  (:require [cljwtang.lib :as cljwtang :refer [info set-db-config!]]
+            [wapp.config :as wappcofig])
   (:gen-class))
 
-(create-app
+(cljwtang/create-app
   (fn []
     (info "set db-config...")
     (set-db-config! wappcofig/db-config)))
